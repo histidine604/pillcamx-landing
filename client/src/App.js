2a58c0528d4b81a38e0227848f1124da50c13463
Tobi Admin <otfadeyi@gmail.com>
2025-08-28 20:40:18 -0400

    Test: Just messing around

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>PillCamx v2025-08-25</h1>
+        <p> Just playing around for now, lol</p>
       </header>
     </div>
   );
